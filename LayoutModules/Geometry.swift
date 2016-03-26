547536c1a540c894729c34081d63b1e710af79f7
Nate Stedman <natesm@gmail.com>
2016-03-26 15:17:06 -0400

    Add geometry data structures.

---
+++ LayoutModules/Geometry.swift
@@ -0,0 +1,167 @@
+// LayoutModules
+// Written in 2016 by Nate Stedman <nate@natestedman.com>
+//
+// To the extent possible under law, the author(s) have dedicated all copyright and
+// related and neighboring rights to this software to the public domain worldwide.
+// This software is distributed without any warranty.
+//
+// You should have received a copy of the CC0 Public Domain Dedication along with
+// this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+import CoreGraphics
+
+// MARK: - Axes
+
+/// The possible major (scrolling) and minor (non-scrolling) axes for a collection view.
+public enum Axis
+{
+    /// The horizontal axis.
+    case Horizontal
+
+    /// The vertical axis.
+    case Vertical
+}
+
+// MARK: - Points
+
+/// A axis-agnostic point structure.
+public struct Point
+{
+    // MARK: - Initialization
+
+    /**
+     Initializes a point structure.
+
+     - parameter major: The major coordinate of the point.
+     - parameter minor: The minor coordinate of the point.
+     */
+    public init(major: CGFloat, minor: CGFloat)
+    {
+        self.major = major
+        self.minor = minor
+    }
+
+    // MARK: - Coordinates
+
+    /// The major coordinate of the point.
+    var major: CGFloat
+
+    /// The minor coordinate of the point.
+    var minor: CGFloat
+}
+
+extension Point
+{
+    // MARK: - Core Graphics Integration
+
+    /**
+     Converts the axis-agnostic point to a Core Graphics point.
+
+     - parameter majorAxis: The major axis to use.
+     */
+    public func CGPointWithMajorAxis(majorAxis: Axis) -> CGPoint
+    {
+        switch majorAxis
+        {
+        case .Horizontal:
+            return CGPoint(x: major, y: minor)
+        case .Vertical:
+            return CGPoint(x: minor, y: major)
+        }
+    }
+}
+
+// MARK: - Sizes
+
+/// A axis-agnostic size structure.
+public struct Size
+{
+    // MARK: - Initialization
+
+    /**
+     Initializes a size structure.
+
+     - parameter major: The major dimension of the size.
+     - parameter minor: The minor dimension of the size.
+     */
+    public init(major: CGFloat, minor: CGFloat)
+    {
+        self.major = major
+        self.minor = minor
+    }
+
+    // MARK: - Coordinates
+
+    /// The major dimension of the size.
+    var major: CGFloat
+
+    /// The minor dimension of the size.
+    var minor: CGFloat
+}
+
+extension Size
+{
+    // MARK: - Core Graphics Integration
+
+    /**
+     Converts the axis-agnostic size to a Core Graphics size.
+
+     - parameter majorAxis: The major axis to use.
+     */
+    public func CGSizeWithMajorAxis(majorAxis: Axis) -> CGSize
+    {
+        switch majorAxis
+        {
+        case .Horizontal:
+            return CGSize(width: major, height: minor)
+        case .Vertical:
+            return CGSize(width: minor, height: major)
+        }
+    }
+}
+
+// MARK: - Rects
+
+/// An axis-agnostic rect structure.
+public struct Rect
+{
+    // MARK: - Initialization
+
+    /**
+     Initializes a rect structure.
+
+     - parameter origin: The origin of the rect.
+     - parameter size:   The size of the rect.
+     */
+    public init(origin: Point, size: Size)
+    {
+        self.origin = origin
+        self.size = size
+    }
+
+    // MARK: - Components
+
+    /// The origin of the rect.
+    var origin: Point
+
+    /// The size of the rect.
+    var size: Size
+}
+
+extension Rect
+{
+    // MARK: - Core Graphics Integration
+
+    /**
+     Converts the axis-agnostic rect to a Core Graphics rect.
+
+     - parameter majorAxis: The major axis to use.
+     */
+    public func CGRectWithMajorAxis(majorAxis: Axis) -> CGRect
+    {
+        return CGRect(
+            origin: origin.CGPointWithMajorAxis(majorAxis),
+            size: size.CGSizeWithMajorAxis(majorAxis)
+        )
+    }
+}
